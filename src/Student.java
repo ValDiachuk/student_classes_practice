8d911c07d82766b16c1572b5bbd0ec75047e08be
Valentyna Diachuk <valdiachuk9@gmail.com>
2023-01-01 22:26:37 -0600

    added some solutions

--- src/Student.java
+++ src/Student.java
@@ -4,9 +4,13 @@ public class Student {
 
 
     //HERE! Create a 5 args constructor
-
-
-
+    public Student(String firstName, String lastName, int age, boolean gender, boolean className) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+        this.className = className;
+    }
     /*
     HERE!
     Create private instance variables as below
@@ -18,14 +22,19 @@ public class Student {
     NOTE: only age will be int and others are String
      */
 
-
+    private String firstName;
+    private String lastName;
+    private int age;
+    private boolean gender;
+    private boolean className;
 
     /*
     HERE!
     Provide public getter method for className only
      */
-
-
+    public boolean isClassName() {
+        return className;
+    }
 
     /*
         HERE!
@@ -33,4 +42,14 @@ public class Student {
         It should be dynamic considering child classes that inherits
      */
 
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                ", className=" + className +
+                '}';
+    }
 }
